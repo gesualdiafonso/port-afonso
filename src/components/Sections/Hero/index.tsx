03e652f7c4b9e1aacf9c61f7caebc0b14beb8e41
gesualdiafonso <gesualdiafonsoarr@outlook.com>
2025-12-03 19:33:17 -0300

    `Removed unused import and simplified useTranslations call`

--- src/components/Sections/Hero/index.tsx
+++ src/components/Sections/Hero/index.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { useTranslations   } from "next-intl"
-import { WelcomeType } from "@/types/i18n/Welcome"
+// import { WelcomeType } from "@/types/i18n/Welcome"
 import ParallaxBacground from "@/components/ui/ParallaxBackground"
 import { SceneWrapper } from "@/components/SceneWrapper"
 
 export default function Hero(){
-    const t = useTranslations<'welcome', WelcomeType>('page.welcome')
+    const t = useTranslations('page.welcome')
     return(
         <section className="h-screen" id="home">
             <SceneWrapper />
