8ac77b2988f0d7a6a2729aadc8c8c94c167a4e0b
gesualdiafonso <gesualdiafonsoarr@outlook.com>
2025-12-04 18:02:43 -0300

    `Fixed typo in CSS class name "felx" to "flex" and added missing closing tag for "div" element`

--- src/components/ui/ProjectDetails/index.tsx
+++ src/components/ui/ProjectDetails/index.tsx
@@ -20,7 +20,7 @@ export default function ProjectDetails ({ title, image, description, list, tech,
     }));    
     
     return(
-        <div className="fixed bg-gray-900/50 inset-0 z-50 felx items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm">
+        <div className="fixed bg-gray-900/50 inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm">
             <motion.div
                 className="relative max-w-2xl border shadow-sm rounded-2xl bg-gray-900 from-midnight to-navy border-white/10"
                 initial={{ opacity: 0, scale: 0.5 }}
@@ -62,30 +62,32 @@ export default function ProjectDetails ({ title, image, description, list, tech,
                         })}
                     </ul>
                     
-                    <a 
-                        className="group relative w-full h-full cursor-pointer overflow-hidden rounded-full border border-gray-200 bg-white px-5 py-2 text-center font-medium text-gray-900 shadow-sm transition-all duration-300 hover:shadow-md dark:border-gray-800 dark:bg-gray-950 dark:text-white dark:hover:border-gray-700" 
-                        href={link}
-                    >
-                        <div className="flex items-center gap-3">
-                            <div
-                                className="h-2 w-2 rounded-full bg-gray-900 transition-all duration-300 group-hover:scale-[100.8] dark:bg-white"
-                            />
-                            <span
-                                className="inline-block transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0"
-                            >
-                            View Project
-                            </span>
-                            <div
-                                 className="absolute top-0 z-10 flex h-full w-full translate-x-12 items-center justify-center gap-3 bg-gray-900 text-white opacity-0 transition-all duration-300 group-hover:-translate-x-5 group-hover:opacity-100 dark:bg-gray-100 dark:text-gray-900"
-                            >
-                                <div className="flex items-center gap-3 whitespace-nowrap">
-                                    <span className="leading-none font-medium">View Project</span>
+                    <div className="mt-5">
+                        <a 
+                            className="group relative cursor-pointer overflow-hidden rounded-full border border-gray-200 bg-white px-5 py-2 text-center font-medium text-gray-900 shadow-sm transition-all duration-300 hover:shadow-md dark:border-gray-800 dark:bg-gray-950 dark:text-white dark:hover:border-gray-700" 
+                            href={link}
+                        >
+                            <div className="flex items-center gap-3">
+                                <div
+                                    className="h-2 w-2 rounded-full bg-gray-900 transition-all duration-300 group-hover:scale-[100.8] dark:bg-white"
+                                />
+                                <span
+                                    className="inline-block transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0"
+                                >
+                                View Project
+                                </span>
+                                <div
+                                    className="absolute top-0 z-10 flex h-full w-full translate-x-12 items-center justify-center gap-3 bg-gray-900 text-white opacity-0 transition-all duration-300 group-hover:-translate-x-5 group-hover:opacity-100 dark:bg-gray-100 dark:text-gray-900"
+                                >
+                                    <div className="flex items-center gap-3 whitespace-nowrap">
+                                        <span className="leading-none font-medium">View Project</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
 
-                    </a>
+                        </a>
+                    </div>
                 </div>
             </motion.div>
         </div>
